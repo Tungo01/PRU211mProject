6f6588c89f1346caaa0a87210be87233cf180f04
Tungo01 <tungmai2k1@gmail.com>
2023-03-05 15:32:06 +0700

    init obstacles

---
+++ Assets/Scripts/Obstacle_Box.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_Box : MonoBehaviour
+{
+    //Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+
+        //pos.x -= player.velocity.x * Time.deltaTime;
+        if (pos.x < -100)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.position = pos;
+    }
+}
