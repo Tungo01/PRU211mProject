a219eeb02aef04ad49b24493832ea791fc57cfc0
Tungo01 <tungmai2k1@gmail.com>
2023-03-06 16:07:10 +0700

    Random box

--- Assets/Scripts/Obstacle_Box.cs
+++ Assets/Scripts/Obstacle_Box.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Obstacle_Box : MonoBehaviour
 {
-    //Player player;
+    Player player;
+    GameObject boxTemplate;
 
     // Start is called before the first frame update
     void Start()
     {
-        //player = GameObject.Find("Player").GetComponent<Player>();
+        player = GameObject.Find("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
@@ -17,10 +18,21 @@ public class Obstacle_Box : MonoBehaviour
     {
         Vector2 pos = transform.position;
 
-        //pos.x -= player.velocity.x * Time.deltaTime;
+        // Van toc cua obstacles
+        pos.x -= player.velocity.x * Time.deltaTime;
         if (pos.x < -100)
         {
             Destroy(gameObject);
+        }
+
+        // Random obstacles
+        int obstacleNum = Random.Range(0, 3);
+        for (int i = 0; i < obstacleNum; i++)
+        {
+            // Tao obstacles
+            GameObject box = Instantiate(boxTemplate.gameObject);
+
+            //Vector2 boxPos;
         }
 
         transform.position = pos;
