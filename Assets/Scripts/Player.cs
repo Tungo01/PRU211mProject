857c6602519213aac1f6a1eec375d1948145fb92
30dejaneiro <iam30dejaneiro@gmail.com>
2023-03-06 07:38:44 +0700

    Holding Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,10 @@ public class Player : MonoBehaviour
     public float jumpVelocity = 20;
     public float groundHeight = 10;
     public bool isGrounded = false;
+    public bool isHoldingJump = false;
+    public float maxHoldJumpTime = 0.4f;
+    public float holdJumpTimer = 0f;
+    public float jumpThreshold = 1; 
     // Start is called before the first frame update
     void Start()
     {
@@ -18,25 +22,46 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (isGrounded)
+    {   Vector2 pos = transform.position;
+        float groundDistance = Mathf.Abs(pos.y-groundHeight); 
+        if (isGrounded || groundDistance <= jumpThreshold)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 isGrounded = false;
                 velocity.y = jumpVelocity;
-                
+                isHoldingJump= true;
+                holdJumpTimer = 0f;
             }
         }
-        Vector2 pos = transform.position;
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isHoldingJump= false;
+        }
+        
         if (!isGrounded)
         {
+            if (isHoldingJump)
+            {
+                holdJumpTimer += Time.deltaTime;
+                if (holdJumpTimer >= maxHoldJumpTime)
+                {
+                    isHoldingJump = false;
+                }
+            }
+
             pos.y += velocity.y * Time.deltaTime;
-            velocity.y += gravity * Time.deltaTime;
+            if (!isHoldingJump)
+            {
+                velocity.y += gravity * Time.deltaTime;
+
+            }
+            
             if (pos.y <= groundHeight)
             {
                 pos.y = groundHeight;
                 isGrounded = true;
+                
             }
         }
         transform.position = pos;
