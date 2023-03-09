d1bd87e5a3a966ca1118a9cf5c12b5f0cd5ce396
Tungo01 <tungmai2k1@gmail.com>
2023-03-09 16:55:07 +0700

    Ren drone vs spikes

--- Assets/Scripts/Obstacle_Spikes.cs
+++ Assets/Scripts/Obstacle_Spikes.cs
@@ -33,7 +33,15 @@ public class Obstacle_Spikes : MonoBehaviour
             // Tao obstacles
             GameObject box = Instantiate(spikesTemplate.gameObject);
 
-            //Vector2 boxPos;
+            // Position Random
+            //float y = goGround.groundHeight;
+            //float halfWidth = goCollider.size.x / 2;
+            //float left = go.transform.position.x - halfWidth + 1;
+            //float right = go.transform.position.x + halfWidth -1;
+            //float x = Random.Range(left, right);
+
+            //Vector2 boxPos = new Vector2(x, y);
+            //box.transform.position = boxPos;
         }
 
         transform.position = pos;
