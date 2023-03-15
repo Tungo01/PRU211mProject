063241efba8413cb37c610129a88e8f249aeb96e
30dejaneiro <iam30dejaneiro@gmail.com>
2023-03-15 15:35:50 +0700

    Add more Function
    Game Over when falling, Menu, game over Menu

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
     Player player;
     Text distanceText;
+    GameObject Result;
+    Text FinalDistance;
     private void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
         distanceText = GameObject.Find("DistanceText").GetComponent<Text>();
+        FinalDistance = GameObject.Find("ScoreText").GetComponent<Text>();
+        Result = GameObject.Find("Result");
+        Result.SetActive(false);
+        
     }
     // Start is called before the first frame update
     void Start()
@@ -23,5 +30,18 @@ public class UIController : MonoBehaviour
     {
         int distance = Mathf.FloorToInt(player.distance);
         distanceText.text = distance + " m";
+        if (player.isDead)
+        {
+            Result.SetActive(true);
+            FinalDistance.text = distance + " m";
+        }
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Retry()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
