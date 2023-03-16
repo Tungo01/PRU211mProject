fedf608566189bd7def80a11e42ad27336e1198a
30dejaneiro <iam30dejaneiro@gmail.com>
2023-03-16 23:07:53 +0700

    abc

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-
+//comment code
 public class Player : MonoBehaviour
 {
     public Animator animator;
